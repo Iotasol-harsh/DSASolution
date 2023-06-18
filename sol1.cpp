87304174fdba60bcf91ec250142c3cf2aa4e2f9a
Pankaj Thakur <pankajvk246@iotasol.com>
2023-06-18 14:48:16 +0530

    Create Sol1

---
+++ sol1.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0)
+            return 0;
+
+        int left = 1;
+        int right = x;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (mid == x / mid)
+                return mid;
+            else if (mid > x / mid)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return right;
+    }
+};
+
+int main() {
+    Solution solution;
+
+    int x = 4;
+    std::cout << solution.mySqrt(x) << std::endl;  // Output: 2
+
+    x = 8;
+    std::cout << solution.mySqrt(x) << std::endl;  // Output: 2
+
+    return 0;
+}
